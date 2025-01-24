2f012798c2690f1b1d04e941e2881ff4472e4ad0
vizualjack <67346906+vizualjack@users.noreply.github.com>
2025-01-25 00:19:57 +0100

    still lags but not totally stucking...

--- customer_app/custom_app/custom_app/starter.h
+++ customer_app/custom_app/custom_app/starter.h
@@ -2,7 +2,7 @@
 #define __STARTER__
 
 #define REAL_STACK(bytes) (bytes/sizeof(StackType_t))
-#define MY_TASK_PRIO 1
+#define MY_TASK_PRIO 20 // 1 works so far the best; 
 extern void my_starter();
 
 #endif
